f27bb6e5ae86f50021ad176fe986527e0275c72d
Mohamed-Firas <150571054+Mohamed-Firas@users.noreply.github.com>
2024-06-25 14:49:19 -0700

    Add files via upload

---
+++ myFuoctions.js
@@ -0,0 +1,13 @@
+
+  document.getElementById("goToPage2").onclick = function() {
+  window.location.href = "AboutUs.html";
+}
+
+document.getElementById("goToPage1").onclick = function() {
+  window.location.href = "Home.html";
+}
+
+document.getElementById("goToPage3").onclick = function() {
+  window.location.href = "page3.html";
+}
+        
